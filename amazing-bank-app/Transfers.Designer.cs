483ee60e4271ca14ea0b98fa055adeb30b5ddf6a
Vtraxio <55654178+Vtraxio@users.noreply.github.com>
2024-12-11 09:37:49 +0100

    Nice Transfer List

--- amazing-bank-app/Transfers.Designer.cs
+++ amazing-bank-app/Transfers.Designer.cs
@@ -28,38 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            bindingSource1 = new BindingSource(components);
-            dataGridView1 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            transactionsList = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label1 = new Label();
+            typeCombo = new ComboBox();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
-            // dataGridView1
+            // transactionsList
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Dock = DockStyle.Fill;
-            dataGridView1.Location = new Point(0, 0);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(800, 450);
-            dataGridView1.TabIndex = 0;
+            transactionsList.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5 });
+            tableLayoutPanel1.SetColumnSpan(transactionsList, 2);
+            transactionsList.Dock = DockStyle.Fill;
+            transactionsList.Location = new Point(3, 32);
+            transactionsList.Name = "transactionsList";
+            transactionsList.Size = new Size(794, 415);
+            transactionsList.TabIndex = 0;
+            transactionsList.UseCompatibleStateImageBehavior = false;
+            transactionsList.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Tytuł";
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Imie";
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Numer Rachunku";
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Kwota";
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Data";
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(transactionsList, 0, 1);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(typeCombo, 1, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Left;
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Typ przlewu";
+            // 
+            // typeCombo
+            // 
+            typeCombo.Anchor = AnchorStyles.Left;
+            typeCombo.DropDownStyle = ComboBoxStyle.DropDownList;
+            typeCombo.FormattingEnabled = true;
+            typeCombo.Items.AddRange(new object[] { "Wysłane", "Otrzymane" });
+            typeCombo.Location = new Point(78, 3);
+            typeCombo.Name = "typeCombo";
+            typeCombo.Size = new Size(121, 23);
+            typeCombo.TabIndex = 2;
+            typeCombo.SelectedIndexChanged += typeCombo_SelectedIndexChanged;
             // 
             // Transfers
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(dataGridView1);
+            Controls.Add(tableLayoutPanel1);
             Name = "Transfers";
             Text = "Transfers";
             Load += Transfers_Load;
-            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
         private BindingSource bindingSource1;
-        private DataGridView dataGridView1;
+        private ListView transactionsList;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
+        private ComboBox typeCombo;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
     }
 }
--- amazing-bank-app/Transfers.cs
+++ amazing-bank-app/Transfers.cs
@@ -10,20 +10,57 @@ using System.Windows.Forms;
 using amazing_bank_app.Network;
 using amazing_bank_app.Network.Dto.Response;
 
-namespace amazing_bank_app
-{
-    public partial class Transfers : Form {
+namespace amazing_bank_app {
+	public partial class Transfers : Form {
 		public Transfers() {
 			InitializeComponent();
 		}
 
 		private void Transfers_Load(object sender, EventArgs e) {
+			typeCombo.SelectedIndex = 0;
+
+			;
+		}
+
+		private void typeCombo_SelectedIndexChanged(object sender, EventArgs e) {
 			Http.Get<TransfersList>("/transfer", out var data);
+			transactionsList.Items.Clear();
 
-			bindingSource1.DataSource = data.sent;
+			var items = new List<ListViewItem>();
 
-			dataGridView1.AutoGenerateColumns = true;
-			dataGridView1.DataSource          = bindingSource1;
-        }
-    }
+			if (typeCombo.SelectedIndex == 0) {
+				var list = data?.sent;
+
+				items = list?.Select(x => {
+					var item = new ListViewItem {
+						Text = x.title
+					};
+                    item.SubItems.Add(x.targetName);
+                    item.SubItems.Add(x.targetId.ToString());
+                    item.SubItems.Add(x.amount.ToString());
+                    item.SubItems.Add(DateTime.Parse(x.date).ToString());
+
+                    return item;
+				}).ToList();
+            } else {
+				var list = data?.received;
+
+				items = list?.Select(x => {
+					var item = new ListViewItem {
+						Text = x.title
+					};
+					item.SubItems.Add(x.targetName);
+					item.SubItems.Add(x.targetId.ToString());
+					item.SubItems.Add(x.amount.ToString());
+					item.SubItems.Add(DateTime.Parse(x.date).ToString());
+
+					return item;
+				}).ToList();
+			}
+
+			transactionsList.Items.AddRange(items.ToArray());
+			transactionsList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+			transactionsList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+		}
+	}
 }
